834b79aa82e1477481644097d22aea916634cff3
mk <mk@MacBook-Air-mk.local>
2025-08-25 16:25:34 +0300

    town_clock.c

---
+++ data-types/town_clock.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int bell = 0;
+	int nBell = 0;
+	
+	scanf("%d %d", &bell, &nBell);
+	int z = nBell - bell;
+
+	int a = bell + ((bell + 1) * (1 <= z)) + ((bell + 2) * (2 <= z)) + ((bell + 3) * (3 <= z)) + ((bell + 4) * (4 <= z)) + ((bell + 5) * (5 <= z)) + ((bell + 6) * (6 <= z)) + ((bell + 7) * (7 <= z)) + ((bell + 8) * (8 <= z)) + ((bell + 9) * (9 <= z)) + ((bell + 10) * (10 <= z)) + ((bell + 11) * (11 <= z)) + ((bell + 12) * (12 <= z));
+	printf("%d\n", a);
+	return 0;
+} 
